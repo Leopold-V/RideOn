175e9527be4687c4cce68d7ed09bd8639d2fd796
Leopold-V <leopoldV79@gmail.com>
2022-08-10 20:42:03 +0200

    fix: jest ReferenceError with import

--- jest-setup-after-env.js
+++ jest-setup-after-env.js
@@ -1,2 +1,6 @@
 import '@testing-library/jest-native/extend-expect';
+jest.mock('@react-navigation/native/lib/commonjs/useLinking.native', () => ({
+    default: () => ({getInitialState: {then: jest.fn()}}),
+    __esModule: true,
+  }));
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
