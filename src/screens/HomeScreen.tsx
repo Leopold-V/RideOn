e3375484a1e8b241feced1268c285fd936105b05
Leopold-V <leopoldV79@gmail.com>
2022-08-13 12:40:56 +0200

    update

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,37 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Button } from 'react-native';
+import { Button, View } from 'react-native';
 import { RootStackParamList } from '@Types/navigation';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Profile'>;
 
 export const HomeScreen = ({ navigation }: Props) => {
+  const updateUser = async () => {
+    const response = await fetch('http://localhost:3001/api/v1/users/01GAB4CF2ZECW5ZPJXXAG2KFVA', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstname: 'John',
+        lastname: 'Doe',
+        password: '123456',
+        email: 'johndoe2@gmail.com',
+        isOnline: true,
+        friendslist: ['1', '5', '7']
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+
+
   return (
-    <Button
-      title="Go to Jane's profile"
-      onPress={() => navigation.navigate('Profile', { name: 'Jane' })}
-    />
+    <View>
+      <Button title="Create a user" onPress={updateUser} />
+      <Button
+        title="Go to Jane's profile"
+        onPress={() => navigation.navigate('Profile', { name: 'Jane' })}
+      />
+    </View>
   );
 };
