f6f5148b26fad2232ea0a73d816c86088d823f43
Leopold-V <leopoldV79@gmail.com>
2022-08-17 23:49:28 +0200

    fix: tests comments

--- exemple.test.js
+++ exemple.test.js
@@ -1,13 +1,13 @@
 import { render } from '@testing-library/react-native';
-import App from './App';
+// import App from './App';
 
 const sum = (a, b) => {
-    return a + b;
-}
+  return a + b;
+};
 
 it('sum 2 numbers', () => {
-    expect(sum(1, 2)).toBe(3);
-})
+  expect(sum(1, 2)).toBe(3);
+});
 
 /*
 test('render app', () => {
